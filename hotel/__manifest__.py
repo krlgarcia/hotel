e1aa3be9277a539a1e311b49ccaa9f4a4437e425
krlgarcia <150921659+krlgarcia@users.noreply.github.com>
2024-05-13 23:57:05 +0800

    Add files via upload

---
+++ hotel/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "hotel",
+    'summary': "Hotel System",
+    'description': "Hotel System",
+    'category': 'Uncategorized',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/charges.xml',
+        'views/guestregistration.xml',
+        'views/guests.xml',
+        'views/mainmenu.xml',
+        'views/rooms.xml',
+        'views/roomtypes.xml',
+    ],
+}
+
---
+++ hotel/models/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+from . import charges,guestregistration,guests,rooms,roomtypes
